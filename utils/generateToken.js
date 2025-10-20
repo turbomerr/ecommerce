f49e7b7a905ed3578e1ddfe83c4a3d40a36569bc
Omer Gokbakar <turbofullstack.dev10@gmail.com>
2025-10-20 17:32:44 +0200

    verify token

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -8,3 +8,8 @@ export const generateJWT = (userId) => {
     const token = jwt.sign({userId}, process.env.JWT_SECRET, {expiresIn : "1h"})
     return token
 }
+
+export const verifyToken = (token) => {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET)
+    console.log(decoded)
+}
