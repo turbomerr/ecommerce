db1f7b859a79db6dbfd361d265a20c468c365f72
Omer Gokbakar <turbofullstack.dev10@gmail.com>
2025-10-21 00:35:47 +0200

    verify token updated

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -5,11 +5,52 @@ dotenv.config()
 
 //create a jwt token and return
 export const generateJWT = (userId) => {
-    const token = jwt.sign({userId}, process.env.JWT_SECRET, {expiresIn : "1h"})
-    return token
+    return jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: "1h" })
+
 }
 
-export const verifyToken = (token) => {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET)
-    console.log(decoded)
-}
+// export const verifyToken = (req, res, next) => {
+//     try {
+//         const token = req.cookies.token;
+//         console.log("req token ", token)
+
+//         if (!token) {
+//             console.log("no token")
+//             return res.status(401).json({ message: "Access denied. No token provided." });
+//         }
+
+//         const decoded = jwt.verify(token, process.env.JWT_SECRET);
+//         req.userId = decoded.userId;
+//         next()
+
+//     } catch (error) {
+//          console.error("Token verification failed:", error);
+//     return res.status(401).json({ message: "Invalid or expired token." });
+//     }
+// }
+
+export const verifyToken = (req, res, next) => {
+    try {
+        let token;
+        const authHeader = req.headers.authorization;
+        if (authHeader && authHeader.startsWith("Bearer ")) {
+            token = authHeader.split(" ")[1]; // "Bearer <token>"
+        }
+
+        if (!token && req.cookies?.token) {
+            token = req.cookies.token;
+        }
+
+        if (!token) {
+            return res.status(401).json({ message: "Access denied. No token provided." });
+        }
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.userId = decoded.userId;
+
+        next();
+    } catch (error) {
+        console.error("Token verification failed:", error.message);
+        return res.status(401).json({ message: "Invalid or expired token." });
+    }
+};
